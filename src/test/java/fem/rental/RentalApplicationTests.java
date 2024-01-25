46647c020f2e7af5462b826eb0e3a303dfef317b
이상훈 <slee@i-magicsoft.com>
2024-01-25 18:10:01 +0900

    first commit

---
+++ src/test/java/fem/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package fem.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
