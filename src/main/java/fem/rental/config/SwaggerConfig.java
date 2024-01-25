81b742921c0a87a36def5e09bb40e215629fb6de
이상훈 <slee@i-magicsoft.com>
2024-01-25 18:14:54 +0900

    swagger 기본 설정

---
+++ src/main/java/fem/rental/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package fem.rental.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("도서 대여 API")
+                .build();
+    }
+}
