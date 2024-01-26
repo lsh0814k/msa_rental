2d81d921310f4886083ab94068ea175d9f6fa5a2
이상훈 <slee@i-magicsoft.com>
2024-01-26 12:17:15 +0900

    Value Object 소스 정리

--- src/main/java/fem/rental/domain/model/vo/IDName.java
+++ src/main/java/fem/rental/domain/model/vo/IDName.java
@@ -1,14 +1,13 @@
 package fem.rental.domain.model.vo;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
-@Getter @Builder
+@Getter
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PRIVATE)
 public class IDName {
--- src/main/java/fem/rental/domain/model/vo/ReturnItem.java
+++ src/main/java/fem/rental/domain/model/vo/ReturnItem.java
@@ -7,14 +7,14 @@ import java.time.LocalDate;
 
 import static lombok.AccessLevel.*;
 
-@Getter @Builder
+@Getter @Builder(access = PRIVATE)
 @AllArgsConstructor(access = PRIVATE)
 @NoArgsConstructor(access = PROTECTED)
 public class ReturnItem {
     private RentalItem rentalItem;
     private LocalDate returnDate;
 
-    private static ReturnItem createReturnItem(RentalItem rentalItem) {
+    public static ReturnItem createReturnItem(RentalItem rentalItem) {
         return ReturnItem
                 .builder()
                 .rentalItem(rentalItem)
