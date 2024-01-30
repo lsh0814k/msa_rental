cd7e0fc5e42622d04261472d28cdad87b4c04468
이상훈 <slee@i-magicsoft.com>
2024-01-30 23:36:24 +0900

    테스트 케이스 추가

--- src/test/java/fem/rental/application/inputport/RentItemInputPortTest.java
+++ src/test/java/fem/rental/application/inputport/RentItemInputPortTest.java
@@ -1,6 +1,7 @@
 package fem.rental.application.inputport;
 
 import fem.rental.UserItemInputDTOFactory;
+import fem.rental.application.usecase.OverdueItemUsecase;
 import fem.rental.domain.model.vo.RentStatus;
 import fem.rental.framework.jpaadapter.RentalCardRepository;
 import fem.rental.framework.web.dto.RentalCardOutputDTO;
@@ -11,12 +12,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class RentItemInputPortTest {
     @Autowired private RentItemInputPort rentItemInputPort;
+    @Autowired private OverdueItemUsecase overdueItemUsecase;
     @Autowired private RentalCardRepository rentalCardRepository;
+    private static int MAX_SIZE = 5;
 
     @BeforeEach
     void init() {
@@ -32,5 +38,32 @@ class RentItemInputPortTest {
         assertThat(rentalCardOutputDTO.getTotalLateFee()).isEqualTo(0);
         assertThat(rentalCardOutputDTO.getRentStatus()).isEqualTo(RentStatus.RENT_AVAILABLE.toString());
         assertThat(rentalCardOutputDTO.getTotalRentCnt()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("도서 대여_5개 이상인 경우 오류")
+    void rent_max_size() {
+        for (int i = 0; i < MAX_SIZE; i++) {
+            rentItemInputPort.rentItem(UserItemInputDTOFactory.create());
+        }
+
+        assertThatThrownBy(() -> rentItemInputPort.rentItem(UserItemInputDTOFactory.create()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .message()
+                .isEqualTo("최대 5권 까지만 대여할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("도서 대여_대여 불가능 상태")
+    void rent_unavailable() {
+        UserItemInputDTO userItemInputDTO = UserItemInputDTOFactory.create();
+        rentItemInputPort.rentItem(userItemInputDTO);
+
+        overdueItemUsecase.overDueItem(userItemInputDTO, LocalDate.now().plusDays(15));
+
+        assertThatThrownBy(() -> rentItemInputPort.rentItem(userItemInputDTO))
+                .isInstanceOf(IllegalArgumentException.class)
+                .message()
+                .isEqualTo("대여 불가 상태입니다.");
     }
 }
